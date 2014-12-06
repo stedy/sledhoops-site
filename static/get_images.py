f4537e8520df86671e7ba5d34006d8dde9457ae7
stedy <zstednic@fhcrc.org>
2014-12-06 20:11:30 +0000

    pulling team logos

---
+++ static/get_images.py
@@ -0,0 +1,17 @@
+import csv
+import urllib2
+
+ids = []
+with open('../lookups.txt', 'rb') as lt:
+    ltreader = csv.reader(lt, delimiter=',', quotechar='"')
+    for row in ltreader:
+        ids.append(row[0])
+
+for id in ids:
+    id = id.lstrip("0")
+    url = "http://hosted.stats.com/cbk/logos/cbk_50x33/" + id + ".png"
+    print url
+    newfile = id + ".png"
+    f = open(newfile, 'wb')
+    f.write(urllib2.urlopen(url).read())
+    f.close()
